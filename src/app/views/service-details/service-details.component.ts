46ab11049fc81713f0c9c06a29503dcb59128c72
Jaiber mauricio Arrieta <mauricioarrieta.dev@gmail.com>
2025-09-14 19:13:15 -0500

    Se crea el servicio - la data y se genera la página de servicios con el buscador funcional

--- src/app/views/service-details/service-details.component.ts
+++ src/app/views/service-details/service-details.component.ts
@@ -1,13 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-import { HeaderComponent } from "../../components/header/header.component";
-import { FooterComponent } from "../../components/footer/footer.component";
 
 @Component({
   selector: 'ts-service-details',
   templateUrl: './service-details.component.html',
   standalone: true,
   styleUrls: ['./service-details.component.css'],
-  imports: [HeaderComponent, FooterComponent]
+  imports: []
 })
 export class ServiceDetailsComponent implements OnInit {
 
