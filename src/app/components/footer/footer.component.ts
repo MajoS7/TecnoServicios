b84fd575ab625579a1932265a758205ae8a377ae
Jaiber mauricio Arrieta <mauricioarrieta.dev@gmail.com>
2025-09-16 07:35:26 -0500

    Se mejoran las imagenes y se enrutan todos los enlaces. Se agrega toast para el botón solicitar servicio

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'ts-footer',
   templateUrl: './footer.component.html',
   standalone: true,
+  imports: [RouterLink],
   styleUrls: ['./footer.component.css']
 })
 export class FooterComponent implements OnInit {
